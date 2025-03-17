7bcb5832c8de1c92ad3ac009ff542923bb8c7f41
omaro <omaroubari16@gmail.com>
2025-03-17 14:54:47 +0300

    fix hero image

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -34,7 +34,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
         </div>
         <div className="aspect-square h-auto w-full select-none">
           {media && typeof media === 'object' && (
-            <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
+            <Media imgClassName="-z-10 object-cover" priority resource={media} />
           )}
         </div>
       </div>
