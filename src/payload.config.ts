20af038d764bf6a3e0c8ff19543ecd469fb94db8
omaro <omaroubari16@gmail.com>
2025-03-22 10:43:11 +0300

    feat(payload-config): add resend email adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { resendAdapter } from '@payloadcms/email-resend'
 
 import sharp from 'sharp' // sharp-import
 import path from 'path'
@@ -134,4 +135,9 @@ export default buildConfig({
     },
     tasks: [],
   },
+  email: resendAdapter({
+    defaultFromAddress: process.env.RESEND_EMAIL || '',
+    defaultFromName: 'Zad WEB',
+    apiKey: process.env.RESEND_API_KEY || '',
+  }),
 })
