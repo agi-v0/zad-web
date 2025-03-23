6c92458dbf7f8fe1b51b8313fbbabaa8eec86568
omaro <omaroubari16@gmail.com>
2025-03-23 11:51:06 +0300

    fix(OG,-hero): fix og image, make hero size with screen

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -16,18 +16,22 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
   // })
 
   return (
-    <div className="bg-zad-green text-white" data-theme="dark">
-      <div className="section relative flex min-h-screen flex-col items-center justify-center lg:flex-row">
+    <div className="bg-zad-green text-white">
+      <div className="section relative flex min-h-[calc(100vh-var(--header-height))] flex-col items-center justify-center lg:flex-row">
         <div className="flex w-full flex-col items-start gap-md">
           {richText && (
-            <RichText className="prose-p:text-body-large" data={richText} enableGutter={false} />
+            <RichText
+              className="prose-p:text-body-large prose-invert"
+              data={richText}
+              enableGutter={false}
+            />
           )}
           {Array.isArray(links) && links.length > 0 && (
             <ul className="flex gap-4 md:justify-center">
               {links.map(({ link }, i) => {
                 return (
                   <li key={i}>
-                    <CMSLink {...link} />
+                    <CMSLink {...link} className="" />
                   </li>
                 )
               })}
